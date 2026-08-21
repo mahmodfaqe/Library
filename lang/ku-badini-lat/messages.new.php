4d42f1b83086c6766f5abb1f50958373208c732c
Scence-bio <hanagull66@gmail.com>
2026-08-22 01:25:20 +0300

    update

---
+++ lang/ku-badini-lat/messages.new.php
@@ -0,0 +1,87 @@
+<?php
+
+return [
+    'university_name' => 'Zanîngeha Raperin',
+    'library_name' => 'Kitêbxaneya Elektronîkî',
+    'site_title' => 'Kitêbxaneya Elektronîkî ya Kolêja Zanistê - Zanîngeha Raperin',
+    'meta_description' => 'Kitêbxaneya Elektronîkî ya Kolêja Zanistê li Zanîngeha Raperin: xizmetguzariyên pirtûkxaneya serhêl û gihîştina pirtûkan û çavkaniyên zanistî bi 8 zimanan.',
+    'jsonld_description' => 'Kitêbxaneya Elektronîkî ya Kolêja Zanistê li Zanîngeha Raperin.',
+    'hero' => [
+        'welcome' => 'Bi xêr hatin',
+        'title' => 'Kitêbxaneya Elektronîkî ya Kolêja Zanistê',
+        'subtitle' => 'Bigihîje hezaran çavkanî û pirtûkên zanistî bi awayekî hêsan û bilez.',
+    ],
+    'intro' => [
+        'heading' => 'Pêşgotin',
+        'paragraphs' => [
+            'Bi xêr hatin bo pirtûkxaneya elektronîkî ya Koleja Zanistê - jîngeheke dîjîtal ku ji bo xwendekar û karmendên akademîk gihîştina agahdarî û fêrbûnê xweştir û lêkolînê jî bikêrtir hatîye sêwirandin.',
+            'Ev pirtûkxaneya elektronîkî cûrbecûr çavkaniyên zanistî pêşkêş dike, di nav de pirtûkên dersê, gotarên zanistî û akademîk, kovarên akademîk, materyalên hînkirinê û referansên perwerdehiyê, hemî li ser platformek hêsan peyda dibin. Ew dihêle ku bikarhêner her dem, li her deverê li agahdariyê bigerin, hem piştgirî dide fêrbûna serbixwe û hem jî çalakiyên lêkolînê yên pêşkeftî. Ev hemû bi rêya kodek QR-ê ya elektronîkî hatî amadekirin.',
+            'Pirtûkxaneya elektronîk ji berhevokeke dîjîtal bêtir e, ew têkiliya di navbera zanist û teknolojiyê de nîşan dide. Ew ramana rexnegir û fêrbûna berdewam teşwîq dike, di heman demê de alîkariya xwendekar û lêkolîneran dike ku li ser pêşkeftinên zanistî yên nûjen agahdar bimînin.',
+            'Erka me avakirina qadeke akademîk a piştgir e ku tê de agahî bi azadî peyda bibin, raman werin pêşxistin, û zanyarên pêşerojê werin hêzdar kirin ku kifş bikin, nûjeniyê bikin û beşdarî pêşveçûna zanist û civakê bibin.',
+        ],
+        'objectives_heading' => 'Armancên Projeyê:',
+        'objectives' => [
+            '🎓 Piştgirîkirina xwendina serhêl li zanko',
+            '⚡ Gihîştina zû û hêsan bo çavkaniyên zanistî',
+            '📖 Berhevkirina hezaran çavkaniyên zanistî di yek cihî de',
+            '📱 Bikaranîna teknolojiya nû :qr',
+            '👨‍🏫 Sûdwergirtina mamoste û xwendekaran ji kitêbxaneya dîjîtal',
+            '⏰ Tijîkirina demên vala yên xwendekaran bi projeyek zanistî',
+            '🌐 Berdestbûna pirtûk û çavkaniyan bi hemû zimanan',
+        ],
+        'qr_label' => 'QR code',
+        'prepared_heading' => 'Amadekirî ji aliyê:',
+        'people' => [
+            [
+                'name' => 'JIYAW YUSIF HUSÊN',
+                'role' => 'Xwendekarê Beşê Biyolojiyê - Dabînkirina pirtûkan',
+            ],
+            [
+                'name' => 'NIYAR QADIR RASÛL',
+                'role' => 'Xwendekarê Beşê Biyolojiyê - Rêxistina kitêbxanê',
+            ],
+            [
+                'name' => 'MEHMÛD XIDIR FAQÊ RASÛL',
+                'role' => 'Xwendekarê Beşê Biyolojiyê - Pêşxistinkarê malperê',
+            ],
+        ],
+    ],
+    'history' => [
+        'heading' => '📚 Dîroka Pirtûkxaneya Koleja Zanistê',
+        'opening_date' => '18/10/2025',
+        'paragraphs' => [
+            'Pirtûkxaneya Koleja Zanistê bi fermana rêzdar Serokê Zanîngehê Profesor Alîkar Dr. Peyman Ramezanê Ehmed hat avakirin, li qata duyemîn a avahiya Koleja Zanistê ya Zanîngeha Raperinê. Di tarîxa :date de ji aliyê Serokê Zanîngehê, Cîgirê wî û Encumena Koleja Zanistê ve hat vekirin û xwendekarên zanistxwaz pêşwazî lê hat kirin.',
+            'Vekirina vê pirtûkxaneyê xalek nû û girîng bû ji bo xwendekar, mamoste û karmendên Koleja Zanistê, û derfet da wan ku bi awayekî hêsantir û bilez bigihîjin pirtûk û çavkaniyên zanistî û akademîk, û pêvajoya fêrbûnê kalîtir û kêrhatîtir bibe.',
+        ],
+    ],
+    'library' => [
+        'heading' => 'Kitêbxaneya Sereke',
+        'button_1' => 'Têkeve Kitêbxaneya Giştî 1',
+        'button_2' => 'Têkeve Kitêbxaneya Giştî 2',
+    ],
+    'dept_heading' => 'Beşên Zanistî',
+    'no_departments' => 'Ti beş nehat dîtin.',
+    'about' => [
+        'heading' => '👥 Derbarê Me',
+        'intro' => 'Wek tîma BioNova ya ji sê xwendekarên zanîngehê pêk tê, em ji bo fêrbûn û teknolojiyê heweseke hevpar parve dikin. Di dema xwendina xwe de, me dît ku dîtina çavkaniyên pêbawer û baş-rêxistinkirî li yek cîhekî pir dijwar e. Ev yek îlham da min ku ez vê pirtûkxaneya elektronîkî biafirînim.',
+        'mission_heading' => 'Mîsyona Me',
+        'mission_text' => '"Deriyê agahdariyê ji bo her xwendekarekî vedike û platformek yekane peyda dike ku hemî hewcedariyên akademîk bi awayekî nûjen û hemdem pêk tîne “',
+    ],
+    'prefooter_note' => 'Ev malpera wek navendek geşesendî, ji aliyê tîma BioNova ve berdewam tê çavdêrîkirin û nûkirin.',
+    'feedback' => [
+        'success' => 'Spas! Rexne û pêşniyara te gihîşt.',
+        'error' => 'Ji kerema xwe rexne an pêşniyara xwe binivîse.',
+        'title' => '💬 Rexne û Pêşniyar',
+        'subtitle' => 'Nêrîna xwe bi me re parve bike ji bo çêtirkirina pirtûkxanê.',
+        'name_label' => 'Nav (dilxwaz)',
+        'message_label' => 'Rexne an pêşniyar',
+        'send' => 'Bişîne',
+    ],
+    'footer' => [
+        'copyright' => 'Kitêbxaneya Elektronîkî ya Kolêja Zanistê - Zanîngeha Raperin  |  Hemû maf parastî ne © :year',
+        'uor_line' => '🎓 :link  |  uor.edu.krd',
+        'uor_link_label' => 'Malpera Zanîngeha Raperîn',
+    ],
+    'visitors_label' => 'Serdankar',
+    'scroll_top' => 'Vegere jor',
+];
